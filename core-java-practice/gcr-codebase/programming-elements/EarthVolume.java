e5c37adf04c1dd1b891b7f65e6ea3f32cdb10000
Aditya Singh <aditya.singh7_cs23@gla.ac.in>
2026-06-29 08:58:52 +0530

    add java code files

---
+++ core-java-practice/gcr-codebase/programming-elements/EarthVolume.java
@@ -0,0 +1,13 @@
+public class EarthVolume {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double pi = 3.141592653589793;
+
+        double volumeKm = (4.0 / 3.0) * pi * radiusKm * radiusKm * radiusKm;
+        double radiusMiles = radiusKm / 1.6;
+        double volumeMiles = (4.0 / 3.0) * pi * radiusMiles * radiusMiles * radiusMiles;
+
+        System.out.println("The volume of earth in cubic kilometers is " + volumeKm);
+        System.out.println("The volume of earth in cubic miles is " + volumeMiles);
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/averagemark.java
@@ -0,0 +1,9 @@
+public class averagemark {
+    public static void main(String[] args) {
+        int m=94;
+        int p=95;
+        int c=96;
+        float average = (m+p+c)*100/300;
+        System.out.println(average);
+    }
+}

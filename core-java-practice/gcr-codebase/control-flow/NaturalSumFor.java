32c64e6a337661d46f5d53ecef79082c6e6f4d43
Aditya Singh <aditya.singh7_cs23@gla.ac.in>
2026-06-29 09:06:21 +0530

    add control flow codes

---
+++ core-java-practice/gcr-codebase/control-flow/NaturalSumFor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NaturalSumFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n >= 1) {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+
+            int formula = n * (n + 1) / 2;
+
+            System.out.println("Sum using for = " + sum);
+            System.out.println("Sum using formula = " + formula);
+
+            if (sum == formula) {
+                System.out.println("Both results are correct.");
+            }
+        } else {
+            System.out.println("Not a natural number.");
+        }
+    }
+}
